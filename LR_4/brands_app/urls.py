85c9981d1ebe3ba3e4ceac9e05ea09aa0fc73c38
Hat1maN <5TheFear5@mail.ru>
2025-11-04 23:43:13 +0500

    urls

--- LR_4/brands_app/urls.py
+++ LR_4/brands_app/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.add_brand, name='add_brand'),         # форма добавления
     path('upload/', views.upload_file, name='upload'),  # загрузка XML
     path('list/', views.list_files, name='list'),       # просмотр всех файлов
+    path('ajax/search/', views.ajax_search, name='ajax_search'),
+    path('edit/<int:pk>/', views.edit_brand, name='edit_brand'),
+    path('delete/<int:pk>/', views.delete_brand, name='delete_brand'),
 ]
+
+
